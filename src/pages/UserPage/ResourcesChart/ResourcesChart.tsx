fa25f5ad689e3aba1a78849d0dbaedaa17665de4
Paulo Endoh <paulo.endoh@gmail.com>
2021-02-11 18:00:47 -0300

    Last year resources!

--- src/pages/UserPage/ResourcesChart/ResourcesChart.tsx
+++ src/pages/UserPage/ResourcesChart/ResourcesChart.tsx
@@ -1,3 +1,10 @@
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@material-ui/core"
 import { DateTime } from "luxon"
 import React, { useEffect, useState } from "react"
 import { connect } from "react-redux"
@@ -6,8 +13,6 @@ import {
   BarChart,
   CartesianGrid,
   Legend,
-  Line,
-  LineChart,
   Tooltip,
   XAxis,
   YAxis,
@@ -20,122 +25,62 @@ import {
   setProfileResources,
 } from "../../../store/profile/profileActions"
 import { ApplicationState } from "../../../store/store"
+import { getLastWeekResourcesForChart } from "../../../utils/feed/getLastWeekResourcesForChart"
+import { getLastYearResourcesForChart } from "../../../utils/feed/getLastYearResourcesForChart"
+
+type ChartRange = "Last week" | "Last year"
+
 // PE 3/3
 const ResourcesChart = (props: Props) => {
   const [lists, setLists] = useState<TagDto[]>([])
-
   const [data, setData] = useState([])
+
+  const [chartRange, setChartRange] = useState<ChartRange>("Last year")
 
   // prepare the graphic
   useEffect(() => {
-    // only recent resources (from yesterday and one week ago)
-    const fromDate = DateTime.local()
-      .plus({ days: -7 })
-      .set({ hour: 0, minute: 0 })
-    const toDate = DateTime.local()
-      .plus({ days: 0 })
-      .set({ hour: 23, minute: 59 })
-    const recentResources = props.resources.filter((r) => {
-      const date = DateTime.fromISO(r.completedAt)
-      return date > fromDate && date < toDate
-    })
-
-    // get list ids
-    const lists: TagDto[] = []
-    const listIds: number[] = []
-    for (const resource of recentResources) {
-      if (resource.tag && !listIds.includes(resource.tag.id)) {
-        listIds.push(resource.tag.id)
-        lists.push(resource.tag)
-      }
+    if (chartRange === "Last week") {
+      const { lists, data } = getLastWeekResourcesForChart(props.resources)
+      setLists(lists)
+      setData(data)
+    } else if (chartRange === "Last year") {
+      const { lists, data } = getLastYearResourcesForChart(props.resources)
+      setLists(lists)
+      setData(data)
     }
-    setLists(lists)
-
-    // select the days
-    const days: DateTime[] = []
-    for (let i = 7; i >= 0; i--) {
-      const day = DateTime.local()
-        .plus({ days: i * -1 })
-        .set({ hour: 0, minute: 0 })
-      days.push(day)
-    }
-
-    // fill the data of each day { name: "Tue", tagId1: 6, tagId3: 4, tagId5: 0 }
-    const data = []
-    for (const day of days) {
-      const obj: any = {
-        name: day.toFormat("ccc"),
-      }
-      for (const list of lists) {
-        obj[list.name] = 0
-      }
-      const resourcesFromDay = recentResources.filter((r) => {
-        const completedAt = DateTime.fromISO(r.completedAt)
-        return completedAt.day === day.day
-      })
-      for (const resource of resourcesFromDay) {
-        if (resource.tag) {
-          obj[resource.tag.name]++
-        }
-      }
-      data.push(obj)
-    }
-    setData(data)
-  }, [props.resources])
+  }, [props.resources, chartRange])
 
   return (
-    // <LineChart
-    //   width={450}
-    //   height={300}
-    //   data={data}
-    //   margin={{
-    //     top: 5,
-    //     right: 30,
-    //     left: 20,
-    //     bottom: 5,
-    //   }}
-    // >
-    //   <CartesianGrid strokeDasharray="3 3" />
-    //   <XAxis dataKey="name" />
-    //   <YAxis />
-    //   <Tooltip />
-    //   <Legend />
-    //   {lists.map((list) => (
-    //     <Line
-    //       key={list.id}
-    //       type="monotone"
-    //       dataKey={list.name}
-    //       stroke={list.color}
-    //     />
-    //   ))}
+    <Box>
+      <FormControl variant="outlined" size="small">
+        <Select
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          value={chartRange}
+          onChange={(e) => setChartRange(e.target.value as ChartRange)}
+        >
+          <MenuItem value={"Last week" as ChartRange}>Last week</MenuItem>
+          <MenuItem value={"Last year" as ChartRange}>Last year</MenuItem>
+        </Select>
+      </FormControl>
 
-    // </LineChart>
-    <BarChart
-      width={450}
-      height={300}
-      data={data}
-      margin={{
-        top: 20,
-        right: 30,
-        left: 20,
-        bottom: 8,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis allowDecimals={false} />
-      <Tooltip />
-      <Legend margin={{ top: 8 }} />
-      {lists.map((list) => (
-        <Bar
-          key={list.id}
-          type="monotone"
-          dataKey={list.name}
-          stackId="a"
-          fill={list.color}
-        />
-      ))}
-    </BarChart>
+      <BarChart width={450} height={300} data={data} margin={{ top: 16 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Legend margin={{ top: 8 }} />
+        {lists.map((list) => (
+          <Bar
+            key={list.id}
+            type="monotone"
+            dataKey={list.name}
+            stackId="a"
+            fill={list.color}
+          />
+        ))}
+      </BarChart>
+    </Box>
   )
 }
 
---
+++ src/utils/feed/getLastWeekResourcesForChart.ts
@@ -0,0 +1,61 @@
+import { DateTime } from 'luxon';
+import { ResourceDto } from '../../interfaces/dtos/relearn/ResourceDto'
+import { TagDto } from '../../interfaces/dtos/relearn/TagDto';
+
+export const getLastWeekResourcesForChart = (resources: ResourceDto[]) => {
+
+
+    // only recent resources
+    const fromDate = DateTime.local()
+        .plus({ days: -7 })
+        .set({ hour: 0, minute: 0 })
+    const toDate = DateTime.local()
+        .plus({ days: 0 })
+        .set({ hour: 23, minute: 59 })
+    const recentResources = resources.filter((r) => {
+        const date = DateTime.fromISO(r.completedAt)
+        return date > fromDate && date < toDate
+    })
+
+    // get list ids
+    const lists: TagDto[] = []
+    const listIds: number[] = []
+    for (const resource of recentResources) {
+        if (resource.tag && !listIds.includes(resource.tag.id)) {
+            listIds.push(resource.tag.id)
+            lists.push(resource.tag)
+        }
+    }
+    lists.concat(lists)
+
+    // select the days
+    const days: DateTime[] = []
+    for (let i = 7; i >= 0; i--) {
+        const day = DateTime.local()
+            .plus({ days: i * -1 })
+            .set({ hour: 0, minute: 0 })
+        days.push(day)
+    }
+
+    // fill the data of each day { name: "Tue", tagId1: 6, tagId3: 4, tagId5: 0 }
+    const data = []
+    for (const day of days) {
+        const obj: any = {
+            name: day.toFormat("ccc"),
+        }
+        for (const list of lists) {
+            obj[list.name] = 0
+        }
+        const resourcesFromDay = recentResources.filter((r) => {
+            const completedAt = DateTime.fromISO(r.completedAt)
+            return completedAt.day === day.day
+        })
+        for (const resource of resourcesFromDay) {
+            if (resource.tag) {
+                obj[resource.tag.name]++
+            }
+        }
+        data.push(obj)
+    }
+    return { data: data, lists: lists }
+}
