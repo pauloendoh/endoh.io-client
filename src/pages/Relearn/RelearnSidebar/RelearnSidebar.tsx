1e7f18d9f367dcd15227c454b2a62c995bd00b5f
Paulo Endoh <your@email.com>
2021-10-17 15:14:41 -0300

    Removing 'untagged' from relearn sidebar

--- src/pages/Relearn/RelearnSidebar/RelearnSidebar.tsx
+++ src/pages/Relearn/RelearnSidebar/RelearnSidebar.tsx
@@ -1,22 +1,10 @@
-import {
-  Box,
-  createStyles,
-  List,
-  ListItem,
-  ListItemText,
-  makeStyles,
-  Theme,
-  Typography,
-} from "@material-ui/core";
-import PATHS from "consts/PATHS";
+import { Box, createStyles, List, makeStyles, Theme } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { Link, useLocation } from "react-router-dom";
 import { Dispatch } from "redux";
 import MySidebar from "../../../components/shared/MySidebar";
 import { TagDto } from "../../../interfaces/dtos/relearn/TagDto";
 import { ApplicationState } from "../../../store/store";
-import { getTodoResources } from "../../../utils/relearn/getTodoResources";
 import RelearnSidebarTagList from "./RelearnSidebarTagList/RelearnSidebarTagList";
 
 function RelearnSidebar(props: Props) {
@@ -27,36 +15,12 @@ function RelearnSidebar(props: Props) {
     setPrivateLists(props.tags.filter((t) => t.isPrivate === true));
   }, [props.tags]);
 
-  const location = useLocation();
-  const isIndex = () => {
-    return location.pathname === PATHS.relearn.index;
-  };
-
-  const classes = useStyles();
-
   return (
     <MySidebar>
       <Box>
         <List disablePadding>
           {/* PE 2/3 - Muito grande para mostrar apenas "Untagged - 16" */}
           {/* criar um <UntaggedLi/> ? */}
-          <ListItem
-            button
-            component={Link}
-            to={PATHS.relearn.index}
-            selected={isIndex()}
-          >
-            <ListItemText>
-              Untagged
-              <Typography variant="inherit" className={classes.resourcesCount}>
-                {
-                  getTodoResources(
-                    props.resources.filter((resource) => resource.tag === null)
-                  ).length
-                }
-              </Typography>
-            </ListItemText>
-          </ListItem>
 
           <RelearnSidebarTagList lists={publicLists} type="public" />
 
