e29cd3092712ab79b070f673caf5cd3da4139d73
Paulo Endoh <your@email.com>
2021-11-08 07:09:55 -0300

    Improved search results preview

--- src/components/navbar/SearchBar/SearchBar.tsx
+++ src/components/navbar/SearchBar/SearchBar.tsx
@@ -1,6 +1,7 @@
-import { Popper } from "@material-ui/core";
+import { Popper, useTheme } from "@material-ui/core";
 import { Autocomplete } from "@material-ui/lab";
 import FlexVCenter from "components/shared/Flexboxes/FlexVCenter";
+import Txt from "components/shared/Text/Txt";
 import useFetchSearchResults from "hooks/react-query/search/useFetchSearchResults";
 import React, { useEffect, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
@@ -10,6 +11,8 @@ import { Dispatch } from "redux";
 import { editResource } from "store/relearn/relearnActions";
 import { ApplicationState } from "store/store";
 import { ResourceDto } from "types/domain/relearn/ResourceDto";
+import { getColorByRating } from "utils/relearn/getColorByRating";
+import Icons from "utils/styles/Icons";
 import pageUrls from "../../../utils/consts/pageUrls";
 import MyTextField from "../../shared/MyInputs/MyTextField";
 
@@ -23,6 +26,7 @@ const MyPopper = function (props: React.ComponentProps<typeof Popper>) {
 
 const SearchBar = ({ editResource }: Props) => {
   const history = useHistory();
+  const [loading, setLoading] = useState(false);
 
   const [throttle, setThrottle] = useState<NodeJS.Timeout>(null);
 
@@ -50,24 +54,57 @@ const SearchBar = ({ editResource }: Props) => {
 
   useEffect(
     () => {
+      if (getValues("searchQuery").length > 0) setLoading(true);
       clearTimeout(throttle);
-      setThrottle(
-        setTimeout(() => {
-          refetch();
-        }, 500)
-      );
+      setThrottle(setTimeout(refetch, 500));
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [watch("searchQuery")]
   );
 
+  useEffect(() => {
+    if (searchResults) setLoading(false);
+  }, [searchResults]);
+
+  const theme = useTheme();
+
   return (
     <form onSubmit={handleSubmit(submit)}>
       <Autocomplete
-        freeSolo
+        loading={loading}
+        // if no text, it won't show "no resources :("
+        freeSolo={watch("searchQuery").length === 0}
+        noOptionsText={"No resources :("}
         options={searchResults?.resources || []}
         PopperComponent={MyPopper}
-        renderOption={(resource) => <FlexVCenter>{resource.title}</FlexVCenter>}
+        renderOption={(resource) => (
+          <FlexVCenter
+            style={{ justifyContent: "space-between", width: "100%" }}
+          >
+            <Txt noWrap style={{ width: 350 }}>
+              {resource.title}
+            </Txt>
+            <FlexVCenter style={{ gap: theme.spacing(2) }}>
+              {resource.rating > 0 && (
+                <FlexVCenter style={{ gap: theme.spacing(0.5) }}>
+                  <Icons.Star
+                    style={{ color: getColorByRating(resource.rating) }}
+                  />
+                  <Txt>{resource.rating}</Txt>
+                </FlexVCenter>
+              )}
+
+              {resource.tag && (
+                <FlexVCenter style={{ gap: theme.spacing(0.5) }}>
+                  <Icons.Label style={{ color: resource.tag.color }} />
+                  <Txt noWrap style={{ width: 125 }}>
+                    {resource.tag.name}
+                  </Txt>
+                </FlexVCenter>
+              )}
+            </FlexVCenter>
+          </FlexVCenter>
+        )}
         getOptionLabel={(resource) => resource.title}
         onChange={(e, resource) => {
           console.log(resource);
