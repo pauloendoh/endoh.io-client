a54913324fc6a294a6a5276fdab6dfea803e6286
Paulo Endoh <your@email.com>
2021-11-08 23:09:12 -0300

    Remove toggle roadmap

--- src/pages/SkillbasePage/SkillDialog/SkillExpectations/SkillExpectations.tsx
+++ src/pages/SkillbasePage/SkillDialog/SkillExpectations/SkillExpectations.tsx
@@ -1,53 +1,34 @@
-import { Box, Button, Collapse, useTheme } from "@material-ui/core";
-import React, { useState } from "react";
-import { connect } from "react-redux";
-import { Dispatch } from "redux";
-import { ApplicationState } from "../../../../store/store";
+import { Box } from "@material-ui/core";
+import Txt from "components/shared/Text/Txt";
+import React from "react";
 import { SkillExpectationDto } from "../../../../types/domain/skillbase/SkillExpectationDto";
 import ExpectationsAtLevel from "./ExpectationsAtLevel/ExpectationsAtLevel";
-const SkillExpectations = (props: Props) => {
-  const [isExpanded, setIsExpanded] = useState(true);
-  const theme = useTheme();
 
-  const levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-  return (
-    <Box mt={2} maxHeight={450}>
-      <Button
-        onClick={() => setIsExpanded(!isExpanded)}
-        fullWidth
-        style={{ background: theme.palette.grey[800] }}
-      >
-        {isExpanded ? "Hide Roadmap" : "Show Roadmap"}
-      </Button>
-
-      <Collapse in={isExpanded} timeout="auto" unmountOnExit>
-        {levels.map((level) => (
-          <ExpectationsAtLevel
-            key={level}
-            expectations={props.expectations}
-            level={level}
-            isHighlighted={props.currentLevel === level}
-            onChangeExpectations={props.onChangeExpectations}
-          />
-        ))}
-      </Collapse>
-    </Box>
-  );
-};
-
-const mapStateToProps = (state: ApplicationState) => ({});
-
-const mapDispatchToProps = (dispatch: Dispatch) => ({});
-
-interface OwnProps {
+interface Props {
   currentLevel: number;
   expectations: SkillExpectationDto[];
   onChangeExpectations: (expectations: SkillExpectationDto[]) => void;
 }
 
-type Props = OwnProps &
-  ReturnType<typeof mapStateToProps> &
-  ReturnType<typeof mapDispatchToProps>;
+// PE 2/3 - rename to SkillRoadmap?
+const SkillExpectations = (props: Props) => {
+  const levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-export default connect(mapStateToProps, mapDispatchToProps)(SkillExpectations);
+  return (
+    <Box mt={2} maxHeight={450}>
+      <Txt variant="h5">Roadmap</Txt>
+
+      {levels.map((level) => (
+        <ExpectationsAtLevel
+          key={level}
+          expectations={props.expectations}
+          level={level}
+          isHighlighted={props.currentLevel === level}
+          onChangeExpectations={props.onChangeExpectations}
+        />
+      ))}
+    </Box>
+  );
+};
+
+export default SkillExpectations;
