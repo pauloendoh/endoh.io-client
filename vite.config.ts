68aadf758f380ca961bfef161c87f4c8b39592fb
Paulo Endoh <your@email.com>
2022-04-30 11:51:33 -0300

    Trying to fix vite 404 in prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import pluginRewriteAll from "vite-plugin-rewrite-all";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths(), react(), pluginRewriteAll()],
 });
