393a7b1e7df8074c6f28326271130fcf48e65513
Paulo Endoh <paulo.endoh@gmail.com>
2022-10-05 23:44:01 -0300

    prettier

--- src/components/Define/DefineSidebar/FileSystem/FileSystem.tsx
+++ src/components/Define/DefineSidebar/FileSystem/FileSystem.tsx
@@ -1,23 +1,23 @@
-import { IconButton, useTheme } from "@material-ui/core";
-import { TreeItem, TreeView } from "@material-ui/lab";
-import DocTitleDialog from "components/Define/DocTitleDialog/DocTitleDialog";
-import Flex from "components/_UI/Flexboxes/Flex";
-import FlexVCenter from "components/_UI/Flexboxes/FlexVCenter";
-import Txt from "components/_UI/Text/Txt";
-import { useFoldersQuery } from "hooks/react-query/folders/useFoldersQuery";
-import useSaveFolderMutation from "hooks/react-query/folders/useSaveFolderMutation";
-import { useEffect, useMemo, useRef, useState } from "react";
-import { useDrop } from "react-dnd";
-import { useParams } from "react-router-dom";
-import useDocsStore from "store/zustand/domain/useDocsStore";
-import useFlashnotesStore from "store/zustand/domain/useFlashnotesStore";
-import { newFolderDto, partialFolderDto } from "types/domain/folder/FolderDto";
-import FolderWithSubfoldersDto from "types/domain/folder/FolderWithSubfoldersDto";
-import Icons from "utils/styles/Icons";
-import FolderDialog from "./FolderDialog/FolderDialog";
-import DocTreeItem from "./FolderTreeItem/DocTreeItem/DocTreeItem";
-import FolderTreeItem from "./FolderTreeItem/FolderTreeItem";
-import { spreadFolders } from "./spreadFolders/spreadFolders";
+import { IconButton, useTheme } from "@material-ui/core"
+import { TreeItem, TreeView } from "@material-ui/lab"
+import DocTitleDialog from "components/Define/DocTitleDialog/DocTitleDialog"
+import Flex from "components/_UI/Flexboxes/Flex"
+import FlexVCenter from "components/_UI/Flexboxes/FlexVCenter"
+import Txt from "components/_UI/Text/Txt"
+import { useFoldersQuery } from "hooks/react-query/folders/useFoldersQuery"
+import useSaveFolderMutation from "hooks/react-query/folders/useSaveFolderMutation"
+import { useEffect, useMemo, useRef, useState } from "react"
+import { useDrop } from "react-dnd"
+import { useParams } from "react-router-dom"
+import useDocsStore from "store/zustand/domain/useDocsStore"
+import useFlashnotesStore from "store/zustand/domain/useFlashnotesStore"
+import { newFolderDto, partialFolderDto } from "types/domain/folder/FolderDto"
+import FolderWithSubfoldersDto from "types/domain/folder/FolderWithSubfoldersDto"
+import Icons from "utils/styles/Icons"
+import FolderDialog from "./FolderDialog/FolderDialog"
+import DocTreeItem from "./FolderTreeItem/DocTreeItem/DocTreeItem"
+import FolderTreeItem from "./FolderTreeItem/FolderTreeItem"
+import { spreadFolders } from "./spreadFolders/spreadFolders"
 
 // PE 1/3 - rename?
 export default function FileSystem() {
@@ -28,20 +28,20 @@ export default function FileSystem() {
     openFolderDialog,
     setOpenFolderDialog,
     folderDialogParentFolderId,
-  } = useFlashnotesStore();
+  } = useFlashnotesStore()
 
-  const docs = useDocsStore((s) => s.docs);
+  const docs = useDocsStore((s) => s.docs)
 
-  const { data: userFolders } = useFoldersQuery();
+  const { data: userFolders } = useFoldersQuery()
 
   const sortedFolders = useMemo(() => {
     if (userFolders?.length > 0) {
-      return userFolders.sort((a, b) => a.name.localeCompare(b.name));
+      return userFolders.sort((a, b) => a.name.localeCompare(b.name))
     }
-    return [];
-  }, [userFolders]);
+    return []
+  }, [userFolders])
 
-  const { mutate: saveFolder } = useSaveFolderMutation();
+  const { mutate: saveFolder } = useSaveFolderMutation()
   const [{ isHovering: folderIsHovering }, dropFolderRef] = useDrop({
     accept: "folder",
 
@@ -52,54 +52,54 @@ export default function FileSystem() {
           name: draggedFolder.name,
           parentFolderId: null,
         })
-      );
+      )
     },
     collect: (monitor) => ({
       isHovering: monitor.isOver(),
     }),
-  });
+  })
 
-  const htmlDropRef = useRef<HTMLDivElement>();
-  dropFolderRef(htmlDropRef);
+  const htmlDropRef = useRef<HTMLDivElement>()
+  dropFolderRef(htmlDropRef)
 
-  const { expandedNodes, toggleNode, setExpandedNodes } = useFlashnotesStore();
+  const { expandedNodes, toggleNode, setExpandedNodes } = useFlashnotesStore()
 
-  const theme = useTheme();
+  const theme = useTheme()
 
   const docsWithoutFolder = useMemo(() => {
-    let filtered = docs.filter((d) => d.folderId === null);
+    let filtered = docs.filter((d) => d.folderId === null)
 
-    return filtered.sort((a, b) => a.title.localeCompare(b.title));
-  }, [docs]);
+    return filtered.sort((a, b) => a.title.localeCompare(b.title))
+  }, [docs])
 
-  const { docId } = useParams<{ docId?: string }>();
+  const { docId } = useParams<{ docId?: string }>()
 
-  const [didFirstExpansion, setDidFirstExpansion] = useState(false);
+  const [didFirstExpansion, setDidFirstExpansion] = useState(false)
   useEffect(() => {
-    if (!docs || sortedFolders.length === 0) return;
-    if (didFirstExpansion) return;
+    if (!docs || sortedFolders.length === 0) return
+    if (didFirstExpansion) return
 
-    let expandNodeIds = ["root"];
+    let expandNodeIds = ["root"]
     if (docId) {
-      let doc = docs.find((d) => d.id === Number(docId));
+      let doc = docs.find((d) => d.id === Number(docId))
 
-      const allFolders = spreadFolders(sortedFolders);
+      const allFolders = spreadFolders(sortedFolders)
 
-      if (!doc) return;
-      let { folderId } = doc;
+      if (!doc) return
+      let { folderId } = doc
 
       while (folderId) {
-        expandNodeIds.push(String(folderId));
+        expandNodeIds.push(String(folderId))
 
-        let folder = allFolders.find((f) => f.id === folderId);
-        debugger;
-        folderId = folder?.parentFolderId;
+        let folder = allFolders.find((f) => f.id === folderId)
+        debugger
+        folderId = folder?.parentFolderId
       }
     }
 
-    setExpandedNodes(expandNodeIds);
-    setDidFirstExpansion(true);
-  }, [docs, sortedFolders, docId]);
+    setExpandedNodes(expandNodeIds)
+    setDidFirstExpansion(true)
+  }, [docs, sortedFolders, docId])
 
   return (
     <Flex style={{ gap: theme.spacing(4) }}>
@@ -114,8 +114,8 @@ export default function FileSystem() {
           defaultChecked
           nodeId="root"
           onClick={(e) => {
-            e.preventDefault();
-            toggleNode("root");
+            e.preventDefault()
+            toggleNode("root")
           }}
           label={
             <div
@@ -135,8 +135,8 @@ export default function FileSystem() {
                   size="small"
                   onClick={(e) => {
                     // impede o toggle
-                    e.preventDefault();
-                    setOpenFolderDialog(true);
+                    e.preventDefault()
+                    setOpenFolderDialog(true)
                   }}
                 >
                   <Icons.CreateNewFolder fontSize="small" />
@@ -173,5 +173,5 @@ export default function FileSystem() {
         onClose={() => setFileDialogParentFolderId(null)}
       /> */}
     </Flex>
-  );
+  )
 }
