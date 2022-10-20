6c83f020d470389d9962146f4a3f4f5869e9a61b
Paulo Endoh <paulo.endoh@gmail.com>
2022-10-20 12:00:13 -0300

    feat/cl8nip4eo183850mtqepg30twd - skill saved!

--- src/hooks/skillbase/useSaveSkill.ts
+++ src/hooks/skillbase/useSaveSkill.ts
@@ -1,46 +1,46 @@
-import useSkillbaseStore from "store/zustand/domain/useSkillbaseStore";
-import useSnackbarStore from "store/zustand/useSnackbarStore";
-import { SkillDto } from "types/domain/skillbase/SkillDto";
-import MyAxiosError from "types/MyAxiosError";
-import myAxios from "utils/consts/myAxios";
-import apiUrls from "utils/url/urls/apiUrls";
+import useSkillbaseStore from "store/zustand/domain/useSkillbaseStore"
+import useSnackbarStore from "store/zustand/useSnackbarStore"
+import { SkillDto } from "types/domain/skillbase/SkillDto"
+import MyAxiosError from "types/MyAxiosError"
+import myAxios from "utils/consts/myAxios"
+import apiUrls from "utils/url/urls/apiUrls"
 
 const useSaveSkill = () => {
   const {
     setSkills,
     setEditingSkill,
     editingSkill: initialValue,
-  } = useSkillbaseStore();
+  } = useSkillbaseStore()
 
-  const { setSuccessMessage, setErrorMessage } = useSnackbarStore();
+  const { setSuccessMessage, setErrorMessage } = useSnackbarStore()
 
   // PE 2/3
   const handleSubmit = (
     skill: SkillDto,
     setSubmitting?: (isSubmitting: boolean) => void
   ) => {
-    if (setSubmitting) setSubmitting(true);
+    if (setSubmitting) setSubmitting(true)
 
     // E.g;: if you change from 5 to ""
-    if ((skill.currentLevel as unknown) === "") skill.currentLevel = null;
-    if ((skill.goalLevel as unknown) === "") skill.goalLevel = null;
+    if ((skill.currentLevel as unknown) === "") skill.currentLevel = null
+    if ((skill.goalLevel as unknown) === "") skill.goalLevel = null
 
     myAxios
       .post<SkillDto[]>(apiUrls.skillbase.skill, skill)
       .then((res) => {
-        setSkills(res.data);
-        setEditingSkill(null);
-        setSuccessMessage("Skill saved successfully!");
+        setSkills(res.data)
+        setEditingSkill(null)
+        setSuccessMessage("Skill saved!")
       })
       .catch((err: MyAxiosError) => {
-        setErrorMessage(err.response.data.errors[0].message);
+        setErrorMessage(err.response.data.errors[0].message)
       })
       .finally(() => {
-        if (setSubmitting) setSubmitting(false);
-      });
-  };
+        if (setSubmitting) setSubmitting(false)
+      })
+  }
 
-  return handleSubmit;
-};
+  return handleSubmit
+}
 
-export default useSaveSkill;
+export default useSaveSkill
