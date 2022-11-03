a530b3c3614f374188eef32a57ee80aaef2c38de
Paulo Endoh <paulo.endoh@gmail.com>
2022-11-03 14:24:19 -0300

    fix/cl9sycchd114090m2b26n845h1 - useAxios error

--- src/hooks/react-query/folders/useDeleteFolderMutation.ts
+++ src/hooks/react-query/folders/useDeleteFolderMutation.ts
@@ -1,28 +1,26 @@
-import { useMutation, useQueryClient } from "react-query";
-import useSnackbarStore from "store/zustand/useSnackbarStore";
-import FolderWithSubfoldersDto from "types/domain/folder/FolderWithSubfoldersDto";
-import myAxios from "utils/consts/myAxios";
-import { urls } from "utils/urls";
-import { queryKeys } from "../queryKeys";
+import { useAxios } from "hooks/utils/useAxios"
+import { useMutation, useQueryClient } from "react-query"
+import useSnackbarStore from "store/zustand/useSnackbarStore"
+import FolderWithSubfoldersDto from "types/domain/folder/FolderWithSubfoldersDto"
+import { urls } from "utils/urls"
+import { queryKeys } from "../queryKeys"
 
 export default function useDeleteFolderMutation() {
-  const { setSuccessMessage, setErrorMessage } = useSnackbarStore();
+  const { setSuccessMessage, setErrorMessage } = useSnackbarStore()
 
-  const queryClient = useQueryClient();
+  const axios = useAxios()
+  const queryClient = useQueryClient()
 
   return useMutation(
     (folderId: number) =>
-      myAxios
+      axios
         .delete<FolderWithSubfoldersDto[]>(urls.api.folderId(folderId))
         .then((res) => res.data),
     {
       onSuccess: (folders) => {
-        queryClient.setQueryData(queryKeys.folders, folders);
-        setSuccessMessage("Folder deleted!");
-      },
-      onError: (err) => {
-        setErrorMessage(JSON.stringify(err));
+        queryClient.setQueryData(queryKeys.folders, folders)
+        setSuccessMessage("Folder deleted!")
       },
     }
-  );
+  )
 }
