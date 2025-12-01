b99b0161519cc2f320df144c98ef13540a28b12d
Paulo Endoh <paulo.endoh@gmail.com>
2025-11-30 21:45:49 -0300

    feat: adds zephyr

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
-import { viteCommonjs } from "@originjs/vite-plugin-commonjs"
-import react from "@vitejs/plugin-react"
-import EnvironmentPlugin from "vite-plugin-environment"
-import pluginRewriteAll from "vite-plugin-rewrite-all"
-import tsconfigPaths from "vite-tsconfig-paths"
-import { defineConfig } from "vitest/config"
-
+import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
+import { withZephyr } from "vite-plugin-zephyr";
+import react from "@vitejs/plugin-react";
+import EnvironmentPlugin from "vite-plugin-environment";
+import pluginRewriteAll from "vite-plugin-rewrite-all";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    react(),
-    pluginRewriteAll(),
-    EnvironmentPlugin("all", { prefix: "REACT_" }),
-    viteCommonjs(),
-  ],
+  plugins: [tsconfigPaths(), react(), pluginRewriteAll(), EnvironmentPlugin("all", {
+    prefix: "REACT_"
+  }), viteCommonjs(), withZephyr()],
   test: {
     environment: "happy-dom",
     dir: "src",
-    setupFiles: "./vitest/globalSetup.ts",
-  },
-})
+    setupFiles: "./vitest/globalSetup.ts"
+  }
+});
